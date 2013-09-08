8b63b5faccb12060c4a8ef10ccf1138ba44b3056
Ekkapob <ekkapob@gmail.com>
2013-09-09 00:47:09 +0700

    Add homepage layout and web fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Scuba::Application.routes.draw do
+  # get "static/home"
+  root "static#home"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
